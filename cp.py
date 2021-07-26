95b94ce2b944438d9853f866bd29b4ca6c70c76c
Taras Savchyn <trsvchn@gmail.com>
2021-07-26 19:28:24 +0300

    Implement crossplatform cp

---
+++ cp.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import sys
+
+
+def main(input_dir, target_dir):
+    os.makedirs(target_dir, exist_ok=True)
+    for fname in os.listdir():
+        if fname.endswith(".gz"):
+            print(f"Copying {fname} to {target_dir}")
+            shutil.copy2(
+                os.path.join(input_dir, fname), os.path.join(target_dir, fname)
+            )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise ValueError("Please specify input and target dirs!")
+    main(sys.argv[1], sys.argv[2])
